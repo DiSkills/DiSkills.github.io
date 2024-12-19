f83c1f10b7127d439c85324a108296c7e85f8132
tokitostr <160491609+tokitostr@users.noreply.github.com>
2024-12-19 18:49:07 +0300

    Update faq.js

--- project/src/faq.js
+++ project/src/faq.js
@@ -1,20 +1,24 @@
-const accordionItems = document.querySelectorAll('.accordion-item');
+document.addEventListener("DOMContentLoaded", function () {
+    const accordionItems = document.querySelectorAll(".accordion-item");
 
-accordionItems.forEach(item => {
-    item.addEventListener('click', () => {
-        accordionItems.forEach(otherItem => {
-            if (otherItem !== item && otherItem.classList.contains('active')) {
-                otherItem.classList.remove('active');
-                otherItem.querySelector('.accordion-content').style.maxHeight = 0;
+    accordionItems.forEach(function(item) {
+        item.addEventListener("click", function() {
+            accordionItems.forEach(function(otherItem) {
+                if (otherItem !== item &&
+                    otherItem.classList.contains("active"))
+                {
+                    otherItem.classList.remove("active");
+                    otherItem.querySelector(".accordion-content").style.maxHeight = 0;
+                }
+            });
+
+            item.classList.toggle("active");
+            const content = item.querySelector(".accordion-content");
+            if (item.classList.contains("active")) {
+                content.style.maxHeight = content.scrollHeight + "px";
+            } else {
+                content.style.maxHeight = 0;
             }
         });
-
-        item.classList.toggle('active');
-        const content = item.querySelector('.accordion-content');
-        if (item.classList.contains('active')) {
-            content.style.maxHeight = content.scrollHeight + "px";
-        } else {
-            content.style.maxHeight = 0;
-        }
     });
 });
